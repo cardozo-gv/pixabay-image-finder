0a0cae3beb5140d460451cc5dc2b2eee3767fe5c
Your Name <griseldavcardozo@gmail.com>
2019-08-18 17:14:50 -0300

    Updates

---
+++ precache-manifest.bacb17be76d486bb6dc7e01693e5dd04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1ad7709c7c6d1fda4a8ef6f53895fb",
+    "url": "/pixabay-image-finder/index.html"
+  },
+  {
+    "revision": "64929a68cd0992d87555",
+    "url": "/pixabay-image-finder/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8db82e8d11edc241c9eb",
+    "url": "/pixabay-image-finder/static/js/2.92036202.chunk.js"
+  },
+  {
+    "revision": "64929a68cd0992d87555",
+    "url": "/pixabay-image-finder/static/js/main.23a1e520.chunk.js"
+  },
+  {
+    "revision": "ebf470f65aa61ae24c0e",
+    "url": "/pixabay-image-finder/static/js/runtime~main.faafb5b5.js"
+  }
+]);
--- src/components/navbar/NavBar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppBar from 'material-ui/AppBar';
-
-
-const NavBar = () => <AppBar title= "PixaBay search finder"/>
-
-
-export default NavBar;
